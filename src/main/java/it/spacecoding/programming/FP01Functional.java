cb45293a81660db5ed75853125da5f37bdf8a7a3
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-22 18:15:57 +0100

    add List example

---
+++ src/main/java/it/spacecoding/programming/FP01Functional.java
@@ -0,0 +1,27 @@
+package it.spacecoding.programming;
+
+import java.util.List;
+
+public class FP01Functional {
+    public static void main(String[] args) {
+        // print all numbers in a list structured approach
+        printAllNumbersInListFunctional(List.of(12, 9, 3, 5, 7, 15));
+
+    }
+    private static void print(int number){
+        System.out.println(number);
+    }
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        // [12,9,3,5,...]
+        // 12
+        // 9
+        // 3
+        // 5
+        // ...
+        // trasformiamo in uno stream
+        // per ogni elemento chiamiamo la stampa
+        // usiamo un riferimento ad un metodo
+        numbers.stream().forEach(FP01Functional::print);
+    }
+   
+}
--- src/main/java/it/spacecoding/programming/FP01Structured.java
+++ src/main/java/it/spacecoding/programming/FP01Structured.java
@@ -6,12 +6,11 @@ public class FP01Structured {
     public static void main(String[] args) {
         // print all numbers in a list structured approach
         printAllNumbersInListStructured(List.of(12, 9, 3, 5, 7, 15));
-
     }
-
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         for (int number : numbers) {
             System.out.println(number);
         }
     }
+    
 }
