8a2d68ce2156db27ee219ed328e2a8592d1b3c34
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-22 17:48:46 +0100

    Initial commit

---
+++ src/main/java/it/spacecoding/programming/FP01Structured.java
@@ -0,0 +1,8 @@
+package it.spacecoding.programming;
+
+public class FP01Structured {
+    public static void main(String[] args) {
+        // print all numbers in a list structured approach
+       // printAllNumbersInListStructured(List.of(12,9,3,5,7,15));
+    }
+}
