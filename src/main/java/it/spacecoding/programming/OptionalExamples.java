8be2d29a47c511def7eb10dad877968b5f69eb7c
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-23 16:52:13 +0100

    added Optional full example

--- src/main/java/it/spacecoding/programming/OptionalExamples.java
+++ src/main/java/it/spacecoding/programming/OptionalExamples.java
@@ -1,5 +1,6 @@
 package it.spacecoding.programming;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 public class OptionalExamples{
     public static void main(String[] args) {
@@ -7,5 +8,15 @@ public class OptionalExamples{
         // Creiamo un predicate per filtrare la nostra lista
         Predicate<? super String> predicate = fruit -> fruit.startsWith("B");
         fruits.stream().filter(predicate).forEach(System.out::println);
+        // Cerchiamo la prima occorrenza e siccome potrebbe non esserci usiamo Optional<>
+        Optional<String> optional = fruits.stream().filter(predicate).findFirst();
+        System.out.println(optional);
+        // metodi utili di Optional
+        System.out.println(optional.isEmpty());
+        System.out.println(optional.isPresent());
+        System.out.println(optional.get());
+        // Best practice da Java 8 per rappresentare i valori vuoti 
+        Optional<String> names = Optional.empty(); 
+        // Non usare : String names = null;
     }
 }
